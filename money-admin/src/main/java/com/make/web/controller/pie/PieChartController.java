229e895199db1e4bc76edd5d053fc9d702d046b9
liangyue2721-lang <liangyue2721@gmail.com>
2026-01-02 22:41:09 +0800

    1

--- money-admin/src/main/java/com/make/web/controller/pie/PieChartController.java
+++ money-admin/src/main/java/com/make/web/controller/pie/PieChartController.java
@@ -1,6 +1,7 @@
 package com.make.web.controller.pie;
 
 import com.alibaba.fastjson2.JSON;
+import com.make.common.utils.SecurityUtils;
 import com.make.finance.domain.vo.LoanTotalRepaymentPieChart;
 import com.make.stock.domain.EtfSalesData;
 import com.make.finance.domain.vo.LoanTotalWithInterestRepaymentPieChart;
@@ -45,127 +46,115 @@ public class PieChartController {
     /**
      * 查询交易类型饼形图数据
      *
-     * @param ids 用户ID数组
      * @return 交易类型饼形图数据
      */
-    @GetMapping("/transactionTypePieChart/{ids}")
-    public List<Map<String, Object>> getTransactionTypePieChart(@PathVariable Long[] ids) {
-        return pieChartService.getTransactionTypePieChartData(ids[0]);
+    @GetMapping("/transactionTypePieChart")
+    public List<Map<String, Object>> getTransactionTypePieChart() {
+        return pieChartService.getTransactionTypePieChartData(SecurityUtils.getUserId());
     }
 
     /**
      * 查询交易总额饼形图数据
      *
-     * @param ids 用户ID数组
      * @return 交易总额饼形图数据
      */
-    @GetMapping("/totalAmountPieChart/{ids}")
-    public List<Map<String, Object>> getTotalAmountPieChart(@PathVariable Long[] ids) {
-        List<Map<String, Object>> totalAmountPieChartData = pieChartService.getTotalAmountPieChartData(ids[0]);
+    @GetMapping("/totalAmountPieChart")
+    public List<Map<String, Object>> getTotalAmountPieChart() {
+        List<Map<String, Object>> totalAmountPieChartData = pieChartService.getTotalAmountPieChartData(SecurityUtils.getUserId());
         JSON.toJSON(totalAmountPieChartData);
-        return pieChartService.getTotalAmountPieChartData(ids[0]);
+        return pieChartService.getTotalAmountPieChartData(SecurityUtils.getUserId());
     }
 
     /**
      * 查询微信支付宝数据
      *
-     * @param ids 用户ID数组
      * @return 微信支付宝数据
      */
-    @GetMapping("/getWechatAlipayData/{ids}")
-    public List<Map<String, Object>> getWechatAlipayData(@PathVariable Long[] ids) {
-        List<Map<String, Object>> totalAmountPieChartData = pieChartService.getWechatAlipayData(ids[0]);
+    @GetMapping("/getWechatAlipayData")
+    public List<Map<String, Object>> getWechatAlipayData() {
+        List<Map<String, Object>> totalAmountPieChartData = pieChartService.getWechatAlipayData(SecurityUtils.getUserId());
         JSON.toJSON(totalAmountPieChartData);
-        return pieChartService.getWechatAlipayData(ids[0]);
+        return pieChartService.getWechatAlipayData(SecurityUtils.getUserId());
     }
 
 
     /**
      * 查询柱形图数据
      *
-     * @param ids 用户ID数组
      * @return 柱形图数据
      */
-    @GetMapping("/totalAmountChart/{ids}")
-    public List<TotalAmount> getTotalAmountChart(@PathVariable Long[] ids) {
-        return pieChartService.getTotalAmountChartData(ids[0]);
+    @GetMapping("/totalAmountChart")
+    public List<TotalAmount> getTotalAmountChart() {
+        return pieChartService.getTotalAmountChartData(SecurityUtils.getUserId());
     }
 
     /**
      * 查询收入/支出柱形图数据
      *
-     * @param ids 用户ID数组
      * @return 收入/支出柱形图数据
      */
-    @GetMapping("/totalMonthlyExpenditureBarChart/{ids}")
-    public List<MonthlyExpenditureBarChart> getMonthlyExpenditureBarChart(@PathVariable Long[] ids) {
-        return pieChartService.getMonthlyExpenditureBarChart(ids[0]);
+    @GetMapping("/totalMonthlyExpenditureBarChart")
+    public List<MonthlyExpenditureBarChart> getMonthlyExpenditureBarChart() {
+        return pieChartService.getMonthlyExpenditureBarChart(SecurityUtils.getUserId());
     }
 
     /**
      * 查询贷款总额偿还比例数据
      *
-     * @param ids 用户ID数组
      * @return 贷款总额偿还比例数据
      */
-    @GetMapping("/repaymentPieChart/{ids}")
-    public List<LoanTotalRepaymentPieChart> getRepaymentPieChart(@PathVariable Long[] ids) {
-        return pieChartService.getRepaymentPieChart(ids[0]);
+    @GetMapping("/repaymentPieChart")
+    public List<LoanTotalRepaymentPieChart> getRepaymentPieChart() {
+        return pieChartService.getRepaymentPieChart(SecurityUtils.getUserId());
     }
 
     /**
      * 查询贷款总额加利息偿还比例数据
      *
-     * @param ids 用户ID数组
      * @return 贷款总额加利息偿还比例数据
      */
     @GetMapping("/totalRepaymentPieChart/{ids}")
-    public List<LoanTotalWithInterestRepaymentPieChart> getTotalRepaymentPieChart(@PathVariable Long[] ids) {
-        return pieChartService.getTotalRepaymentPieChart(ids[0]);
+    public List<LoanTotalWithInterestRepaymentPieChart> getTotalRepaymentPieChart() {
+        return pieChartService.getTotalRepaymentPieChart(SecurityUtils.getUserId());
     }
 
     /**
      * 查询当月消费收入比
      *
-     * @param ids 用户ID数组
      * @return 当月消费收入比数据
      */
-    @GetMapping("/getMonthIncomeExpenseRatio/{ids}")
-    public List<LoanTotalWithInterestRepaymentPieChart> getMonthIncomeExpenseRatio(@PathVariable Long[] ids) {
-        return pieChartService.getMonthIncomeExpenseRatio(ids[0]);
+    @GetMapping("/getMonthIncomeExpenseRatio")
+    public List<LoanTotalWithInterestRepaymentPieChart> getMonthIncomeExpenseRatio() {
+        return pieChartService.getMonthIncomeExpenseRatio(SecurityUtils.getUserId());
     }
 
     /**
      * 查询当年消费收入比
      *
-     * @param ids 用户ID数组
      * @return 当年消费收入比数据
      */
-    @GetMapping("/getYearIncomeExpenseRatio/{ids}")
-    public List<LoanTotalWithInterestRepaymentPieChart> getYearIncomeExpenseRatio(@PathVariable Long[] ids) {
-        return pieChartService.getYearIncomeExpenseRatio(ids[0]);
+    @GetMapping("/getYearIncomeExpenseRatio")
+    public List<LoanTotalWithInterestRepaymentPieChart> getYearIncomeExpenseRatio() {
+        return pieChartService.getYearIncomeExpenseRatio(SecurityUtils.getUserId());
     }
 
     /**
      * 获取利润折线图数据
      *
-     * @param ids 用户ID数组
      * @return 利润折线图数据
      */
-    @GetMapping("/getProfitLineData/{ids}")
-    public List<SalesData> getProfitLineData(@PathVariable Long[] ids) {
-        System.out.println(ids.length);
-        return pieChartService.getProfitLineData(ids[0]);
+    @GetMapping("/getProfitLineData")
+    public List<SalesData> getProfitLineData() {
+        return pieChartService.getProfitLineData(SecurityUtils.getUserId());
     }
 
     /**
      * 获取多个 ETF 的利润折线图数据，优化查询性能
      *
-     * @param ids 用户ID数组
      * @return 包含多个 ETF 销售数据的列表，用于绘制折线图
      */
-    @GetMapping("/getETFProfitLineData/{ids}")
-    public List<EtfSalesData> getETFProfitLineData(@PathVariable Long[] ids) {
+    @GetMapping("/getETFProfitLineData")
+    public List<EtfSalesData> getETFProfitLineData() {
         long startTime = System.currentTimeMillis();
         // 提取所有代码用于批量查询
         List<String> etfCodes = new ArrayList<>();
@@ -182,23 +171,21 @@ public class PieChartController {
     /**
      * 查询收入柱形图数据
      *
-     * @param ids 用户ID数组
      * @return 收入柱形图数据
      */
-    @GetMapping("/getMonthlyIncomeBarChart/{ids}")
-    public List<MonthlyExpenditureBarChart> getMonthlyIncomeBarChart(@PathVariable Long[] ids) {
-        return pieChartService.getMonthlyIncomeBarChart(ids[0]);
+    @GetMapping("/getMonthlyIncomeBarChart")
+    public List<MonthlyExpenditureBarChart> getMonthlyIncomeBarChart() {
+        return pieChartService.getMonthlyIncomeBarChart(SecurityUtils.getUserId());
     }
 
     /**
      * 查询还贷柱形图数据
      *
-     * @param ids 用户ID数组
      * @return 还贷柱形图数据
      */
-    @GetMapping("/renderLoanRepaymentComparisonChart/{ids}")
-    public List<MonthlyExpenditureBarChart> renderLoanRepaymentComparisonChart(@PathVariable Long[] ids) {
-        return pieChartService.renderLoanRepaymentComparisonChart(ids[0]);
+    @GetMapping("/renderLoanRepaymentComparisonChart")
+    public List<MonthlyExpenditureBarChart> renderLoanRepaymentComparisonChart() {
+        return pieChartService.renderLoanRepaymentComparisonChart(SecurityUtils.getUserId());
     }
 
     /**
@@ -207,8 +194,8 @@ public class PieChartController {
      *
      * @return 许可证检查信息的JSON字符串
      */
-    @GetMapping("/GetLicenseCheck/")
-    public String renderLoanRepaymentComparisonChart() {
+    @GetMapping("/GetLicenseCheck")
+    public String GetLicenseCheck() {
         return pieChartService.getLicenseCheck();
     }
 }
