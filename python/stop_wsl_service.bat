90d0cd84068c0015e65bbbf652f94ac7d40876dd
liangyue2721-lang <liangyue2721@gmail.com>
2026-01-30 09:42:22 +0800

    Delete stop_wsl_service.bat

--- python/stop_wsl_service.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ---------------------------------------
-echo Stopping stock service in WSL2...
-echo ---------------------------------------
-
-wsl -d Ubuntu -- pkill -f gunicorn
-
-echo Done.
-pause
